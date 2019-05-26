19304abbddb80aa571112fd42a89fce80af6a742
Dustin Michels <dustin7538@gmail.com>
2019-05-25 21:50:04 -0600

    clean up csv2json

--- README.md
+++ README.md
@@ -11,3 +11,10 @@ See:
 - ["How to Say (Almost) Everything in a Hundred-Word Language" (The Atlantic)](https://www.theatlantic.com/technology/archive/2015/07/toki-pona-smallest-language/398363/)
 
 - ["Allusionist 25: Toki Pona" (The Allusionist Podcast)](https://www.theallusionist.org/allusionist/tokipona)
+
+## Attribution
+
+Linja Pona font:
+
+- Creatd by:[Jan Same](https://github.com/janSame/linja-pona)
+- Upgraded by [Davidar](https://github.com/davidar/linja-pona)
--- csv2json.py
+++ csv2json.py
@@ -1,27 +1,20 @@
 import csv
 import json
 
-words = {}
+words: dict = {}
 fp = "assets/data/words.json"
 
-with open('toki_pona_dict.csv', newline='') as csvfile:
+with open("toki_pona_dict.csv", newline="") as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-
-        w = row['word']
-
-        definition = {
-            'pos': row['pos'],
-            'def': row['definition'],
-            'eg': row['example']}
-
-        if not words.get(w):
-            words[w] = {
-                "word": w,
-                "definitions": [definition]}
+        word = row["word"]
+        definition = {"pos": row["pos"], "def": row["definition"], "eg": row["example"]}
+        if words.get(word):
+            words[word]["definitions"].append(definition)
         else:
-            words[w]['definitions'].append(definition)
+            words[word] = {"word": word, "definitions": [definition]}
+
 
 wordsList = list(words.values())
-with open(fp, 'w') as f:
+with open(fp, "w") as f:
     json.dump(wordsList, f, indent=2)
